1f4cf0a18364a9f31867c6b32a7755f05962540b
Vaibhav <vaibhavsiroha2000@gmail.com>
2025-04-29 23:13:03 +0530

    Add main app configuration, models, views, and templates for ContactsApp

--- ContactsApp/main/models.py
+++ ContactsApp/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    Name=models.CharField(max_length=200)
+    Email=models.EmailField(max_length=200)
+    number=models.CharField(max_length=15)
--- ContactsApp/main/views.py
+++ ContactsApp/main/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from .models import Contact
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def ContactBar(request):
+    names = Contact.objects.all()
+    context={'names':names}
+    return render(request,context)
